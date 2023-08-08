99d91f66c72c5530e8395a2ed277a837f9d005df
Richard Vodden <richard@humanisingautonomy.com>
2023-08-08 13:51:49 +0100

    Allow MultiRecivers to process CommandLists of a subset of interfaces

--- src/catapult/test/test_command.cpp
+++ src/catapult/test/test_command.cpp
@@ -21,27 +21,42 @@ TEST (TestReverseCommand, ExecuteTest) {
   underTest.execute (receiver);
 }
 
-class MockInterface {};
+class MockInterface1 {};
+class MockInterface2 {};
 
-class MockReceiver: public Receiver<MockInterface> {};
+class UnderTestMultiReceiver : public MultiReceiver<MockInterface1, MockInterface2> {};
 
-class UnderTestMultiReceiver : public MultiReceiver<MockInterface> {};
-
-class MockCommand : public Executable<MockInterface> {
+class MockCommand1 : public Executable<MockInterface1> {
   public:
-    MOCK_METHOD (void, execute, (MockInterface& receiver), (const override));
+    MOCK_METHOD (void, execute, (MockInterface1& receiver), (const override));
 };
 
-TEST(TestReceiver, TestExecuteCommandList) {
-  auto mockCommand1 = std::make_shared<MockCommand>();
-  auto mockCommand2 = std::make_shared<MockCommand>();
-  CommandList<MockInterface> commandList {
+class MockCommand2 : public Executable<MockInterface2> {
+
+  public:
+    MOCK_METHOD (void, execute, (MockInterface2& receiver), (const override));
+};
+
+TEST(TestMultiReceiver, TestExecuteCommandList) {
+  auto mockCommand1 = std::make_shared<MockCommand1>();
+  auto mockCommand2 = std::make_shared<MockCommand2>();
+  CommandList<MockInterface1, MockInterface2> commandList {
     mockCommand1,
     mockCommand2
   };
   UnderTestMultiReceiver underTest;
   EXPECT_CALL(*mockCommand1, execute);
   EXPECT_CALL(*mockCommand2, execute);
+  underTest.applyCommandList(commandList);
+}
+
+TEST(TestMultiReceiver, TestExecuteCommandListAimedAtASubsetOfInterfaces) {
+  auto mockCommand1 = std::make_shared<MockCommand1>();
+  CommandList<MockInterface1> commandList {
+    mockCommand1,
+  };
+  UnderTestMultiReceiver underTest;
+  EXPECT_CALL(*mockCommand1, execute);
   underTest.applyCommandList(commandList);
 }
 
